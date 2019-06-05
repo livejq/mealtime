66f184e3fc112dd1631409dd45ba73fa644041d7
livejq <1325286933@qq.com>
2019-06-05 20:54:00 +0800

    Initial commit

---
+++ src/main/java/com/livejq/mealtime/MealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.livejq.mealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealtimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livejq/mealtime/LoggerTest.java
@@ -0,0 +1,27 @@
+package com.livejq.mealtime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@Slf4j
+//@Data
+public class LoggerTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLogger() {
+        String name = "zhl";
+        String password = "1234";
+
+//        logger.debug("debug..."); 默认输出的日志级别为info以上，所以此debug将不会输出
+        logger.info("info...name = {}, password = {}", name, password);
+        logger.error("error...");
+    }
+}
